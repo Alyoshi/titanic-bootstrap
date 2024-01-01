e739a2b4e74ee3c05b7cf03a7e168def9d683e5a
Bastian Sattelberger <bastian.sattelberger@gmail.com>
2024-01-01 22:47:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function calculateProbability() {
+    // Gather user input
+    const pclass = document.getElementById('pclass').value;
+    const female = document.getElementById('female').value;
+    const age = document.getElementById('age').value;
+    const fare = document.getElementById('fare').value;
+
+    // Prepare data for the API request
+    const requestData = {
+        Pclass: parseInt(pclass),
+        Female: JSON.parse(female),
+        Age: parseInt(age),
+        Fare: parseInt(fare),
+    };
+    console.log(requestData);
+    console.log(JSON.stringify(requestData));
+    // Make the API request
+    fetch('https://titanic-survival-probability.onrender.com/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Display the result to the user
+        const resultContainer = document.getElementById('result');
+        resultContainer.innerHTML = `<h4>Berechnete Überlebenswahrscheinlichkeit beträgt ${data.prediction * 100}%</h4>`;
+    })
+    .catch(error => {
+        // Display an error message to the user
+        const resultContainer = document.getElementById('result');
+        resultContainer.innerHTML = '<p>An error occurred while fetching the data. Please try again later.</p>';
+    });
+}
